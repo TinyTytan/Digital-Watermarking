4354e20359a31fa24344ae4b464cc996c284eafc
Theo Rickman <theorickman@mail.com>
2021-12-14 00:54:46 +0000

    DHdec: add file loading

--- DHdec.m
+++ DHdec.m
@@ -4,3 +4,16 @@
 
 clear;
 
+% % Load image
+[filename,path] = uigetfile('*.jpg');
+imgO = imread([path,filename]);
+
+if strcmp(filename,'sun.jpg') || strcmp(filename,'birds.jpg')
+   imgO = rot90(imgO,-1); % otherwise images will be landscape
+end
+
+imgDb = im2double(imgO); % convert to double
+img = rgb2ycbcr(imgDb);  % convert to YCbCr
+
+aspectRatio = [size(img,2),size(img,1),size(img,3)];
+
