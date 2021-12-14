f42c6fc6520a5b745f62c96bb7be6a53e6d15806
Theo Rickman <theorickman@mail.com>
2021-12-14 22:39:18 +0000

    DHdec: add more useful existing code, update comments

--- DHdec.m
+++ DHdec.m
@@ -4,7 +4,7 @@
 
 clear;
 
-% % Load image
+% % Load watermarked image
 [filename,path] = uigetfile('*.jpg');
 imgO = imread([path,filename]);
 
@@ -16,4 +16,11 @@ imgDb = im2double(imgO); % convert to double
 img = rgb2ycbcr(imgDb);  % convert to YCbCr
 
 aspectRatio = [size(img,2),size(img,1),size(img,3)];
+
+imgY = squeeze(img(:,:,1)); % extract luma part
+
+% % decompose Y part of watermarked image using dwt
+[LL,HL,LH,HH] = dwt2(imgY,waveletType);
+[LL2,HL2,LH2,HH2] = dwt2(LL,waveletType);
+[LL3,HL3,LH3,HH3] = dwt2(LL2,waveletType);
 
