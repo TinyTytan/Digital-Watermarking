6765ce445e7f599e0f72c717dff35f3bdf428721
Theo Rickman <theorickman@mail.com>
2021-12-15 00:22:31 +0000

    fix DH watermark sizing

--- dataHide.m
+++ dataHide.m
@@ -1,8 +1,8 @@
 function out = dataHide(matrix,watermark)
-    if size(matrix,1)/3 ~= size(watermark,1) || size(matrix,2) ~= size(watermark,2)
+    if size(matrix,1) ~= size(watermark,1) || size(matrix,2)/3 ~= size(watermark,2)
         disp("Error- watermark incorrect size, or matrix dimensions not divisible by 3");
     else
-        for workingRow = 1:size(matrix,1)/3 % add redundancy
+        for workingRow = 1:size(matrix,1) % add redundancy
             for workingColumn = 1:size(matrix,2)/3
                 columnLoc = 3*workingColumn-2;
                 activeSection = matrix(workingRow,columnLoc:columnLoc+2);
