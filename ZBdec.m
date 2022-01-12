d0b2e33c288653a092a1cac55c65e8f9e84e6a7e
Theo Rickman <theorickman@mail.com>
2022-01-12 23:53:48 +0000

    add ZBdec, program to generate watermark from image and key

---
+++ ZBdec.m
@@ -0,0 +1,31 @@
+% Data-Hiding Decoder
+% By Theo Rickman
+% University of Sheffield 2021
+
+% clear;
+waveletType = 'haar';
+
+% Load watermarked image
+[filename,path] = uigetfile('*.jpg');
+imgO = imread([path,filename]);
+
+imgDb = im2double(imgO); % convert to double
+img = rgb2ycbcr(imgDb);  % convert to YCbCr
+imgY = squeeze(img(:,:,1)); % extract luma part
+
+% % decompose Y part of watermarked image using dwt
+[LL,HL,LH,HH]     = dwt2(imgY,waveletType);
+[LL2,HL2,LH2,HH2] = dwt2(LL,waveletType);
+
+% extract watermark from watermarked matrices
+HH2ex = keyComp(HH2,key(1));
+HL2ex = keyComp(HL2,key(2));
+LH2ex = keyComp(LH2,key(3));
+
+% % measurements/validation
+% compare extracted and original watermark- 1 is ideal
+
+load('watermarkZB.mat');
+disp(['HL2ex Correctness == ',num2str(hammingf(HL2ex,watermarkZB))])   % compare HL2
+disp(['LH2ex Correctness == ',num2str(hammingf(LH2ex,watermarkZB))])   % compare LH2
+disp(['HH2ex Correctness == ',num2str(hammingf(HH2ex,watermarkZB))])   % compare HH2
