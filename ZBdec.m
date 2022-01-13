1ece16af74a09f9c0726c1d06550cdbd3e452ea1
Theo Rickman <theorickman@mail.com>
2022-01-13 00:28:18 +0000

    ZBdec: rearrange, fix bug from incorrectly referring to key

--- ZBdec.m
+++ ZBdec.m
@@ -18,14 +18,14 @@ imgY = squeeze(img(:,:,1)); % extract luma part
 [LL2,HL2,LH2,HH2] = dwt2(LL,waveletType);
 
 % extract watermark from watermarked matrices
-HH2ex = keyComp(HH2,key(1));
-HL2ex = keyComp(HL2,key(2));
-LH2ex = keyComp(LH2,key(3));
+HH2ex = keyComp(HH2,key(:,:,1));
+HL2ex = keyComp(HL2,key(:,:,2));
+LH2ex = keyComp(LH2,key(:,:,3));
 
 % % measurements/validation
 % compare extracted and original watermark- 1 is ideal
 
 load('watermarkZB.mat');
+disp(['HH2ex Correctness == ',num2str(hammingf(HH2ex,watermarkZB))])   % compare HH2
 disp(['HL2ex Correctness == ',num2str(hammingf(HL2ex,watermarkZB))])   % compare HL2
 disp(['LH2ex Correctness == ',num2str(hammingf(LH2ex,watermarkZB))])   % compare LH2
-disp(['HH2ex Correctness == ',num2str(hammingf(HH2ex,watermarkZB))])   % compare HH2
