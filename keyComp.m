4de0f0b83a10d97b3a839305a7ffd4898337b959
Theo Rickman <theorickman@mail.com>
2022-01-14 16:30:26 +0000

    keyComp, keyGen: use simpler and more effective imbinarize method

--- keyComp.m
+++ keyComp.m
@@ -1,17 +1,7 @@
 function watermarkEx = keyComp(matrix,key)
-    mid = median(matrix,'all');
-    feat = matrix;
-    for workingRow = 1:size(matrix,1)
-        for workingColumn = 1:size(matrix,2)
-            if matrix(workingRow,workingColumn) >= mid
-                feat(workingRow,workingColumn) = 1;
-            elseif matrix(workingRow,workingColumn) < mid
-                feat(workingRow,workingColumn) = 0;
-            else
-                disp("Uh-oh");
-            end
-        end
+    if size(matrix,1) ~= size(watermark,1) || size(matrix,2) ~= size(watermark,2)
+        disp("Error- watermark incorrect size");
+    else
+        watermarkEx = xor(imbinarize(matrix), key);
     end
-    watermarkEx = xor(feat,key);
 end
-
