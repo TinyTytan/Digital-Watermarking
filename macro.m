ea2b4ae9a8b2a054fc7c829add41d89133cbce8d
Theo Rickman <theorickman@mail.com>
2022-03-21 19:49:15 +0000

    macro: add final generation lines

--- macro.m
+++ macro.m
@@ -13,14 +13,35 @@ mkdir(new_path);
 copyfile(strcat(origin_path,'\',filename,ext),strcat(new_path,'\',filename,'_',ext));
 filename = strcat(filename,'_');
 
-attack_Android(new_path,filename,ext);                  % generate _a
-attack_Android(new_path,strcat(filename,"a"),ext);      % generate _aa
-attack_Android(new_path,strcat(filename,"aa"),ext);     % generate _aaa
+attack_Android(new_path,filename,ext);                      % generate _a
+attack_Android(new_path,strcat(filename,"a"),ext);          % generate _aa
+attack_Android(new_path,strcat(filename,"aa"),ext);         % generate _aaa
+    
+attack_IOS(new_path,filename,ext);                          % generate _i
+attack_IOS(new_path,strcat(filename,"i"),'.png');           % generate _ii
+attack_IOS(new_path,strcat(filename,"ii"),'.png');          % generate _iii
+    
+attack_JPEG(new_path,filename,ext);                         % generate _p
+attack_JPEG(new_path,strcat(filename,"p"),ext);             % generate _pp
+attack_JPEG(new_path,strcat(filename,"pp"),ext);            % generate _ppp
+    
+attack_Android(new_path,strcat(filename,"p"),ext);          % generate _pa
+attack_IOS(new_path,strcat(filename,"p"),ext);              % generate _pi
+    
+attack_Android(new_path,strcat(filename,"pp"),ext);         % generate _ppa
+attack_IOS(new_path,strcat(filename,"pp"),ext);             % generate _ppi
+    
+attack_JPEG(new_path,strcat(filename,"a"),ext);             % generate _ap
+attack_JPEG(new_path,strcat(filename,"ap"),ext);            % generate _app
+attack_JPEG(new_path,strcat(filename,"i"),'.png');          % generate _ip
+attack_JPEG(new_path,strcat(filename,"ip"),ext);            % generate _ipp
 
-attack_IOS(new_path,filename,ext);                      % generate _i
-attack_IOS(new_path,strcat(filename,"i"),'.png');       % generate _ii
-attack_IOS(new_path,strcat(filename,"ii"),'.png');      % generate _iii
+attack_IOS(new_path,strcat(filename,"a"),ext);              % generate _ai
+attack_Android(new_path,strcat(filename,"i"),'.png');       % generate _ia
 
-attack_JPEG(new_path,filename,ext);                     % generate _p
-attack_JPEG(new_path,strcat(filename,"p"),ext);         % generate _pp
-attack_JPEG(new_path,strcat(filename,"pp"),ext);        % generate _ppp
+attack_IOS(new_path,strcat(filename,"aaa"),ext);            % generate _aaai
+attack_IOS(new_path,strcat(filename,"aaai"),'.png');        % generate _aaaii
+attack_IOS(new_path,strcat(filename,"aaaii"),'.png');       % generate _aaaiii
+attack_JPEG(new_path,strcat(filename,"aaaiii"),'.png');     % generate _aaaiiip
+attack_JPEG(new_path,strcat(filename,"aaaiiip"),ext);       % generate _aaaiiipp
+attack_JPEG(new_path,strcat(filename,"aaaiiipp"),ext);      % generate _aaaiiippp
