e63d394930dfd7c5c60cacfbd7bac6501522d3ee
Theo Rickman <theorickman@mail.com>
2022-03-21 13:40:05 +0000

    attack_IOS: fix typos, output PNG

--- attack_IOS.m
+++ attack_IOS.m
@@ -1,6 +1,6 @@
 function attack_IOS(img_in,filepath)
 %ATTACK_IOS Emulates iOS screenshot attack
-%   Resize image, then JPG compress it
+%   Resize image, then PNG compress it
 
     % dimensions for resizing
 % res = [2778 1284]; % iPhone 12/13 Pro Max
@@ -12,13 +12,13 @@ phone_asp = res(2)/res(1);
 image_res = size(img_in);
 image_asp = image_res(2)/image_res(1);
 
-if image_asp < phone_asp % if the image's aspect ratio  is less than the phone's
+if image_asp < phone_asp % if the image's aspect ratio is less than the phone's
     % then the image is bound by height
     res(2) = res(1)*image_asp; % set image width
 else % the image is bound by width
     res(1) = res(2)*1/image_asp; % set image height
 end
 
-imwrite(imresize(img_in,res),filepath);
+imwrite(imresize(img_in,res),strcat(filepath,"I.png"));
 
 end
