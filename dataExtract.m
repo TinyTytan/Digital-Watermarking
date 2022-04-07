e6a1634ae27863cee1d28813255b7a385dc41761
Theo Rickman <theorickman@mail.com>
2022-04-07 16:31:12 +0100

    dataExtract: fully remove debugging features

--- dataExtract.m
+++ dataExtract.m
@@ -12,21 +12,10 @@ function watermarkEx = dataExtract(matrix)
 
             if activeSection(index) < asMedian % if the exceptional value is less than the median, extract 1
                 watermarkEx(workingRow,workingColumn) = 1;
-                if watermark(workingRow,workingColumn) == 0 % if this was the incorrect value, flag error
-                    failex_0 = failex_0 + 1;
-                else
-                    numsucc = numsucc + 1;
-                end
             elseif activeSection(index) > asMedian % if the exceptional value is greater than the median, extract 0
                 watermarkEx(workingRow,workingColumn) = 0; 
-                if watermark(workingRow,workingColumn) == 1 % if this was the incorrect value, flag error
-                    failex_1 = failex_1 + 1;
-                else
-                    numsucc = numsucc + 1;
-                end
             else % if all values are the same, extract 0 and flag error
                 watermarkEx(workingRow,workingColumn) = 0;
-                tfailex = tfailex + 1;
             end
         end
     end
