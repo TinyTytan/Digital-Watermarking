853fd42f50237f8ad558fc59c8b89c4316f1b3ae
Theo Rickman <theorickman@mail.com>
2022-04-08 15:13:49 +0100

    keyGen: add watermark resizing from DH

--- keyGen.m
+++ keyGen.m
@@ -1,7 +1,22 @@
 function key = keyGen(matrix,watermark)
-    if size(matrix,1) ~= size(watermark,1) || size(matrix,2) ~= size(watermark,2)
-        disp("Error- watermark incorrect size");
-    else
-        key = xor(imbinarize(matrix), watermark);
-    end
+cRows = size(matrix,1);
+cCols = size(matrix,2);
+
+constrSquare = min(cRows,cCols);
+watermark = imresize(watermark,[constrSquare constrSquare]);
+
+if cRows < cCols % if matrix is wide
+    watermark = repmat(watermark, 1, 30);
+    watermark = imcrop(watermark, [0 0 cCols constrSquare]);
+
+elseif cCols < cRows % if matrix is tall
+    watermark = repmat(watermark, 30, 1);
+    watermark = imcrop(watermark, [0 0 constrSquare cRows]);
+
+end
+
+save('ZBlastusedwatermark.mat','watermark');
+
+key = xor(imbinarize(matrix), watermark);
+
 end
