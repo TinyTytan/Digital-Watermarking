91828167e87b8b9ad1dd97f7fe4abd9216c164e1
Theo Rickman <theorickman@mail.com>
2022-05-14 07:24:19 +0100

    dataExtract/Hide: use new median_matrix method, adjust edge case handling, clean up

--- dataExtract.m
+++ dataExtract.m
@@ -1,20 +1,25 @@
 function watermarkEx = dataExtract(matrix)
-    watermarkEx = NaN(size(matrix,1),size(matrix,2)/3);
+    cRows = size(matrix,1);
+    cCols = size(matrix,2)/3;
 
-    for workingRow = 1:size(matrix,1)
-        for workingColumn = 1:size(matrix,2)/3 % for each set of 3 columns
+    watermarkEx = NaN(cRows,cCols);
+
+    median_Matrix = reshape(median(reshape(matrix', 3, [])),cCols,[])';
+
+    for workingRow = 1:cRows
+        for workingColumn = 1:cCols % for each set of 3 columns
 
             columnLoc = 3*workingColumn-2; % first element in each set of 3
             activeSection = matrix(workingRow,columnLoc:columnLoc+2); % pull out array of the working elements
 
-            asMedian = median(activeSection);
+            asMedian = median_Matrix(workingRow,workingColumn);
             [~, index] = max(abs(activeSection - asMedian)); % find index of value furthest from median
 
             if activeSection(index) < asMedian % if the exceptional value is less than the median, extract 1
                 watermarkEx(workingRow,workingColumn) = 1;
             elseif activeSection(index) > asMedian % if the exceptional value is greater than the median, extract 0
                 watermarkEx(workingRow,workingColumn) = 0; 
-            else % if all values are the same, extract 0 and flag error
+            else % if all values are the same, extract 0
                 watermarkEx(workingRow,workingColumn) = 0;
             end
         end
--- dataHide.m
+++ dataHide.m
@@ -1,5 +1,10 @@
 function out = dataHide(matrix,watermark)
-watermark = wm_sizer(size(matrix,1),size(matrix,2),watermark,'dh');
+cRows = size(matrix,1);
+cCols = size(matrix,2)/3;
+
+watermark = wm_sizer(cRows,cCols*3,watermark,'dh');
+
+median_Matrix = reshape(median(reshape(matrix', 3, [])),cCols,[])';
 
 for workingRow = 1:cRows
 
@@ -7,24 +12,23 @@ for workingRow = 1:cRows
 
         columnLoc = 3*workingColumn-2;
         activeSection = matrix(workingRow,columnLoc:columnLoc+2);
-        oddOneOut = abs(activeSection-median(activeSection));
+        oddOneOut = abs(activeSection-median_Matrix(workingRow,workingColumn));
         [~, index] = min(oddOneOut);
 
-        if max(oddOneOut)-min(oddOneOut) <= abs(mean(activeSection))*0.01
+        if max(oddOneOut) - min(oddOneOut) <= 1/255
             if watermark(workingRow,workingColumn) == 1
-                matrix(workingRow,columnLoc+1) = activeSection(index)-1e-18-abs(activeSection(1)*0.1);
+                matrix(workingRow,columnLoc+1) = activeSection(index)-1/255;
             elseif watermark(workingRow,workingColumn) == 0
-                matrix(workingRow,columnLoc+1) = activeSection(index)+1e-18+abs(activeSection(1)*0.1);
+                matrix(workingRow,columnLoc+1) = activeSection(index)+1/255;
             end
         elseif watermark(workingRow,workingColumn) == 1
             matrix(workingRow,columnLoc+index-1) = max(activeSection);
-        elseif watermark(workingRow,workingColumn) == 0
-            matrix(workingRow,columnLoc+index-1) = min(activeSection);
         else
-            disp("Uh-oh");
+            matrix(workingRow,columnLoc+index-1) = min(activeSection);
         end
+
     end
+
 end
 out = matrix;
-
 end
